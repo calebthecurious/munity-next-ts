581096448482899d41175d672cd8a994870b069b
calebthecurious <calebthecurious@gmail.com>
2023-04-16 03:43:43 +1000

    4th commit - Registration UI

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -108,6 +108,8 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+
+                {footer}
               </div>
             </div>
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/navbar/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Munity",
@@ -21,7 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hello World" isOpen />
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
