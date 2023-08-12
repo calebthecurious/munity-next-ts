f653be1602d9e49283b87fe41f0cd3a636e43336
calebtc <calebthecurious@gmail.com>
2023-08-12 16:43:52 +0700

    25th commit - Auth & Registration UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IlistingParams } from "./actions/getListings";
 
@@ -5,6 +6,7 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+import AuthForm from "./components/AuthForm";
 
 interface HomeProps {
   searchParams: IlistingParams;
@@ -23,32 +25,74 @@ const Home = async ({ searchParams }: HomeProps) => {
   }
 
   return (
-    <ClientOnly>
-      <Container>
-        <div
+    <div
+      className="
+        flex
+        min-h-full
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+      "
+    >
+      <div
+        className="
+            sm:mx-auto
+            sm:w-full
+            sm:max-w-md
+          "
+      >
+        <Image
+          alt="Logo"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+        />
+        <h2
           className="
-            pt-24 
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4 
-            xl:grid-cols-5 
-            2xl:grid-cols-6 
-            gap-8"
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          "
         >
-          {listings.map((listing: any) => {
-            return (
-              <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            );
-          })}
-        </div>
-      </Container>
-    </ClientOnly>
+          Sign in to your account
+        </h2>
+      </div>
+      <AuthForm />
+    </div>
+
+    // <ClientOnly>
+    //   <Container>
+    //     <div
+    //       className="
+    //         pt-24
+    //         grid
+    //         grid-cols-1
+    //         sm:grid-cols-2
+    //         md:grid-cols-3
+    //         lg:grid-cols-4
+    //         xl:grid-cols-5
+    //         2xl:grid-cols-6
+    //         gap-8"
+    //     >
+    //       {listings.map((listing: any) => {
+    //         return (
+    //           <ListingCard
+    //             currentUser={currentUser}
+    //             key={listing.id}
+    //             data={listing}
+    //           />
+    //         );
+    //       })}
+    //     </div>
+    //   </Container>
+    // </ClientOnly>
   );
 };
 
